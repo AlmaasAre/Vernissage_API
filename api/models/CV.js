c12fb09aeb05a09580b1f9712b21d671165be859
tcskrovseth <thoskr@gmail.com>
2013-11-08 17:29:41 +0100

    Work in progress

---
+++ api/models/CV.js
@@ -0,0 +1,19 @@
+/**
+ * CV
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+  	attributes: {
+
+  		email: 'string',
+  		available: 'boolean',
+  		text: 'string',
+  		projects:
+
+  	}
+};
---
+++ api/models/Content.js
@@ -0,0 +1,18 @@
+/**
+ * Content
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+  	attributes: {
+
+  		title: 'string',
+  		description: 'string',
+  		url: 'string',
+  		video: 'boolean'
+  	}
+};
