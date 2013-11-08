58d42137d50ced426630ee33ae6e0319b0942353
tcskrovseth <thoskr@gmail.com>
2013-11-08 10:47:14 +0100

    Initial user model

---
+++ api/controllers/UsersController.js
@@ -0,0 +1,13 @@
+/**
+ * UsersController
+ *
+ * @module		:: Controller
+ * @description	:: Contains logic for handling requests.
+ */
+
+module.exports = {
+
+	sayHello: function (req, res) {
+		res.send('hello world!');
+	}
+};
